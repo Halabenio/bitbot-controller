0607f2dedbfc17db716ea0bf03787d1dd0c12955
Halabenio <beniamin.halaburda@halabenio.com>
2026-07-10 10:11:23 +0100

    Connection Prep

--- main.ts
+++ main.ts
@@ -3,76 +3,38 @@ enum RadioMessage {
     CarKeepalive = 29201,
     ControllerKeepalive = 30085
 }
-radio.onReceivedMessage(RadioMessage.CarKeepalive, function () {
-    CarKeepalive = 1
-    LastKeepalive = 25
-})
-radio.onReceivedMessage(RadioMessage.ControllerKeepalive, function () {
-    ControllerKeepalive = 1
-})
-input.onGesture(Gesture.Shake, function () {
-    if (Connected == -1) {
-        Connected = 0
+input.onButtonPressed(Button.A, function () {
+    if (Status == 1) {
+        SettingNumber += -1
+        if (SettingNumber < 0) {
+            SettingNumber = 20
+        }
+        basic.showNumber(SettingNumber)
     }
 })
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Connected = -1
-})
-let LastKeepalive = 0
-let ControllerKeepalive = 0
-let CarKeepalive = 0
-let Connected = 0
-Connected = -1
-CarKeepalive = 0
-ControllerKeepalive = 0
-let Channel = 0
-basic.showNumber(Channel)
-radio.setGroup(40)
-music.play(music.stringPlayable("C E G - - - - - ", 500), music.PlaybackMode.InBackground)
-basic.forever(function () {
-    if (Connected == -1) {
-        basic.showLeds(`
-            # . # . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . # . #
-            `)
-    } else if (Connected == 0) {
-        if (Channel < 9) {
-            Channel += 1
-        } else {
-            Channel = 0
+input.onButtonPressed(Button.B, function () {
+    if (Status == 1) {
+        SettingNumber += 1
+        if (SettingNumber > 20) {
+            SettingNumber = 0
         }
-        radio.setGroup(40 + Channel)
-        basic.showNumber(Channel)
-        CarKeepalive = 0
-        ControllerKeepalive = 0
-        basic.pause(500)
-        if (CarKeepalive == 1 && ControllerKeepalive == 0) {
-            Connected = 1
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                # . . . #
-                . # # # .
-                `)
-            music.play(music.stringPlayable("- D E C A - - - ", 500), music.PlaybackMode.UntilDone)
-        }
-    } else if (Connected == 1) {
-        radio.sendMessage(RadioMessage.ControllerKeepalive)
-        if (input.buttonIsPressed(Button.B)) {
-            radio.sendString("Left")
-        } else {
-            radio.sendString("StopLeft")
-        }
-        if (input.buttonIsPressed(Button.A)) {
-            radio.sendString("Right")
-        } else {
-            radio.sendString("StopRight")
-        }
-    } else if (Connected == 2) {
-    	
+        basic.showNumber(SettingNumber)
     }
 })
+let SettingNumber = 0
+let Gateway = 0
+let Status = 0
+Status = 0
+// Unset
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+if (flashstorage.getOrDefault("Gateway", "NotSet") != "NotSet") {
+    Status = 2
+    Gateway = parseFloat(flashstorage.getOrDefault("Gateway", "0"))
+    radio.setGroup(Gateway * 10)
+}
